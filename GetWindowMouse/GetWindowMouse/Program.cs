5ea627ce4a995d35f07c1994bf0cfa1643861252
sitenik <sitenik@gmail.com>
2016-03-11 21:19:36 +0300

    1

--- GetWindowMouse/GetWindowMouse/Program.cs
+++ GetWindowMouse/GetWindowMouse/Program.cs
@@ -16,7 +16,9 @@ namespace GetWindowMouse
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            Обработка тест = new Обработка();
+            тест.Do();
         }
     }
 }
