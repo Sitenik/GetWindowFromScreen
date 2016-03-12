45f2b0b42eba446d22fc56ad424826fe0b0e570e
sitenik <sitenik@gmail.com>
2016-03-12 13:15:34 +0300

    end

--- GetWindowMouse/GetWindowMouse/Program.cs
+++ GetWindowMouse/GetWindowMouse/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GetWindowMouse
@@ -16,7 +13,6 @@ namespace GetWindowMouse
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
             Обработка тест = new Обработка();
             тест.Do();
         }
