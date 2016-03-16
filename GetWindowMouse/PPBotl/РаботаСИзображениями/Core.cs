a35b8a0b694b167f03fd2b087f78f0370f5419e0
sitenik <sitenik@gmail.com>
2016-03-16 20:36:30 +0300

    12

---
+++ GetWindowMouse/PPBotl/РаботаСИзображениями/Core.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using GetMainWin;
+using WinApiCom;
+
+namespace PPBotl.РаботаСИзображениями
+{
+    public class Core : IDisposable
+    {
+        protected InfoHwnd _ОкноПриложения;
+        public InfoHwnd ОкноПриложения 
+        {
+            get 
+            {
+                return _ОкноПриложения;
+            }
+        }
+
+        public Core() 
+        {
+            /// Конструктор
+        }
+
+        private bool Init() 
+        {
+            try
+            {
+                /// Инициализация
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(String.Format("{0}:{1}", this.GetType().FullName, exc.Message));
+                return false;
+            }
+            return true;
+        }
+
+
+        public void ВыбратьОкноПриложения() 
+        {
+            using (ОбработкаВыбораОкна ОкноПриложенияHelper = new ОбработкаВыбораОкна()) 
+            {
+                _ОкноПриложения = ОкноПриложенияHelper.Do();
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
